765714be861e8718677fb31e19519f26d8180309
Hanbalkhan775 <hanbalkhan999@gmail.com>
2025-09-27 13:14:31 +0100

    Add Day_26

--- Day_26/main.py
+++ Day_26/main.py
@@ -1,1 +1,12 @@
-new_names = [name.title() for name in names if len(name) > 5]
+import pandas as pd
+
+df = pd.read_csv("nato_phonetic_alphabet.csv")
+
+phnoetic_dict = {row.letter: row.code for (index, row) in df.iterrows()}
+
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+word = input("Enter a word: ").upper()
+output_list = [phnoetic_dict[letter] for letter in word]
+print(output_list)
+
