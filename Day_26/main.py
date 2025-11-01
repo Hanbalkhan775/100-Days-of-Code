541fc20bdd5b0f858fb7f22edb01b721c88e7a54
Hanbalkhan775 <hanbalkhan999@gmail.com>
2025-11-01 03:09:01 +0000

    added exception handling to the project

--- Day_26/main.py
+++ Day_26/main.py
@@ -6,7 +6,14 @@ phnoetic_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ").upper()
-output_list = [phnoetic_dict[letter] for letter in word]
-print(output_list)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phnoetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
 
+generate_phonetic()
